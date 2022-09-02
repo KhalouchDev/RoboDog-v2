dee4de04dd0092d12d2e465961dad825ad4d7a6e
Khaled-Murrar <khalouchdev@gmail.com>
2022-09-02 15:28:48 +0300

    Motion Filter

--- roboDog_static/Filter.ino
+++ roboDog_static/Filter.ino
@@ -1,3 +1,6 @@
-void filter() {
-  
+// to filter filter motion
+
+float filter(float prevValue, float currentValue, int filter) {
+  float lengthFiltered = (prevValue + (currentValue * filter)) / (filter + 1);
+  return lengthFiltered;
 }
