5e65ca9598eec10fe390f2d680192a2c220ccba3
Khaled-Murrar <khalouchdev@gmail.com>
2022-09-03 22:33:26 +0300

    Code & Cad
    Z-axis inverse kinematics
    New side design
    New shoulder design

--- roboDog_static/Kinematics.ino
+++ roboDog_static/Kinematics.ino
@@ -1,3 +1,10 @@
 void kinematics() {
-  
+  C = (sq(a) + sq(b) - sq(c)) / (2*a*b);
+  C = acos(C);
+  C = (C * 180 / PI);
+
+  B = A = ((180-C)/2);
+
+  shoulder.write(B);
+  knee.write(C);
 }
--- roboDog_static/roboDog_static.ino
+++ roboDog_static/roboDog_static.ino
@@ -1,7 +1,21 @@
+#include<Servo.h>
+
+int a = 8;
+int b = 8;
+float c;
+
+float A;
+float B;
+float C;
+
+Servo shoulder;
+Servo knee;
+
 void setup() {
-  
+  shoulder.attach(10);
+  knee.attach(9);
 }
 
 void loop() {
-  
+  kinematics();
 }
